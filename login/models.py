d7beb18235b7968176070f16be004a715d42c8f7
Farleygood <44248556@qq.com>
2018-05-25 19:33:52 +0800

    Initial commit

---
+++ login/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models her.
+
+from django.db import models
+
+class User(models.Model):
+
+    gender = (
+        ('mail', '男'),
+        ('female', '女'),
+    )
+
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    sex = models.CharField(max_length=32, choices=gender, default='男')
+    c_time = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-c_time"]
+        verbose_name = "用户"          # 给模型起一个名字
+        verbose_name_plural = "用户"   # 模型的复数形式
